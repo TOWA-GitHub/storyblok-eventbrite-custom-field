58cad9fdcc7c700986406fb3a0d56eef173d72dc
Thai Phan <minh-thai.phan@towa.at>
2023-10-19 14:54:10 +0200

    feat: add src

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import EventbritePlugin from './components/EventbritePlugin/index.vue';
+import { FieldPluginProvider } from '@storyblok/field-plugin/vue3'
+</script>
+
+<template>
+  <FieldPluginProvider>
+    <template v-slot:isLoading><span>Loading...</span></template>
+    <template v-slot:error><span>Error</span></template>
+
+    <EventbritePlugin />
+  </FieldPluginProvider>
+</template>
